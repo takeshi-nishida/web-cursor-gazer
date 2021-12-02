34b39f40bb2f8d75c9a64b0b6d2a6f5f7d78ba00
takeshi-nishida <takeshi-nishida@users.noreply.github.com>
2021-12-02 17:21:17 +0900

    Record and replay mouse movement

--- src/content_script.js
+++ src/content_script.js
@@ -1,10 +1,10 @@
 console.log("running webgazer-chrome-extension...");
 
-webgazer.setGazeListener((data, time) => {
-    if(!data) return;
+// webgazer.setGazeListener((data, time) => {
+//     if(!data) return;
 
-    console.log("(" + data.x + "," + data.y + ") @" + time);
-});
+//     console.log("(" + data.x + "," + data.y + ") @" + time);
+// });
 
 //webgazer.begin();
 
@@ -15,17 +15,63 @@ document.body.appendChild(spotlight);
 chrome.storage.local.get({
     spotlightSize: 200,
 }, (data) => {
-
     document.addEventListener("mousemove", e => {
-        const background = `background: radial-gradient(
-            circle at ${e.clientX}px ${e.clientY}px,
-            transparent,
-            transparent ${data.spotlightSize}px,
-            rgba(0, 0, 0, 0.6) ${data.spotlightSize * 2}px)`
-        spotlight.setAttribute("style", background);
+        spotlight.setAttribute("style", createSpotlight(e.clientX, e.clientY, data.spotlightSize));
     });
-
 })
 
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if(!sender.tab){ // message was from popup.js
+        switch(request.type){
+            case "start recording": startRecording() ; break;
+            case "stop recording": stopRecording() ; break;
+            case "replay": replay() ; break;
+            case "log": break;
+        }
+        sendResponse(cursorLog);
+    }
+});
 
+let cursorLog;
 
+function recordMouse(e){
+    cursorLog.push({ x: e.clientX, y: e.clientY, t: e.timeStamp });
+}
+
+function startRecording(){
+    console.log("start recording");
+    cursorLog = [];
+    document.addEventListener("mousemove", recordMouse);
+}
+
+function stopRecording(){
+    console.log("stop recording");
+    document.removeEventListener("mousemove", recordMouse);
+}
+
+function replay(){
+    console.log("replay");
+
+    let i = 0;
+
+    replayImpl = () => {
+        if(i < cursorLog.length){
+            const e = cursorLog[i];
+            spotlight.setAttribute("style", createSpotlight(e.x, e.y, 200));
+            if(i + 1 < cursorLog.length){
+                const next = cursorLog[++i];
+                setTimeout(replayImpl, next.t - e.t);
+            }
+        }
+    };
+
+    replayImpl();
+}
+
+function createSpotlight(x, y, size){
+    return `background: radial-gradient(
+        circle at ${x}px ${y}px,
+        transparent,
+        transparent ${size}px,
+        rgba(0, 0, 0, 0.6) ${size * 2}px)`
+}
---
+++ src/popup.js
@@ -0,0 +1,56 @@
+const startRecordingButton = document.getElementById("startRecording");
+const stopRecordingButton = document.getElementById("stopRecording");
+const replayButton = document.getElementById("replay");
+const csvButton = document.getElementById("prepareCSV");
+const csvLink = document.getElementById("csv");
+
+startRecordingButton.addEventListener("click", startRecording);
+stopRecordingButton.addEventListener("click", stopRecording);
+replayButton.addEventListener("click", replay);
+prepareCSV.addEventListener("click", requestLog);
+
+async function startRecording(){
+    const tab = await getCurrentTab();
+    chrome.tabs.sendMessage(tab.id, { type: "start recording" }, res => console.log(res));
+}
+
+async function stopRecording(){
+    const tab = await getCurrentTab();    
+    chrome.tabs.sendMessage(tab.id, { type: "stop recording" }, res => console.log(res));
+}
+
+async function replay(){
+    const tab = await getCurrentTab();    
+    chrome.tabs.sendMessage(tab.id, { type: "replay" }, res => console.log(res));
+}
+
+async function requestLog(){
+    const tab = await getCurrentTab();    
+    chrome.tabs.sendMessage(tab.id, { type: "log" }, res => csv(res));
+}
+
+async function getCurrentTab(){
+    let queryOptions = { active: true, currentWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+}
+
+function csv(data) {
+    const content = object2csv(data);
+    const bom = new Uint8Array([0xEF, 0xBB, 0xBF]);
+    const blob = new Blob([bom, content], { "type": "text/csv" });
+    csvLink.href = window.URL.createObjectURL(blob);
+    csvLink.style.visibility = "visible";
+}
+
+function object2csv(json) {
+    var header = Object.keys(json[0]).join(',') + "\n";
+
+    var body = json.map(function(d){
+        return Object.keys(d).map(function(key) {
+            return d[key];
+        }).join(',');
+    }).join("\n");
+
+    return header + body;
+}
